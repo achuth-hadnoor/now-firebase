81c05b4f9766f923c79762d83ca4bf525b4143df
achuth-hadnoor <achuth.hadnoor123@gmail.com>
2020-09-21 05:50:27 +0000

    added logo for the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
-import Theme from '../utils/theme.js'
-import {AuthProvider} from '../lib/auth.js'
+import Header from '@/components/Header.js'
+import { AuthProvider } from '@/lib/auth.js'
+import Theme from '@/utils/theme'
 export default function App({ Component, pageProps }) {
   return (
     <> 
     <AuthProvider>
         <Theme>
+            <Header/>
             <Component {...pageProps} /> 
         </Theme>
     </AuthProvider>
--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,7 @@ export default function Home() {
     const auth = useAuth();
   return (
       <div>
-      {
-        auth.loading ? 
-            <div>loading</div> : 
-            auth.user ? 
-            <button 
-                 onClick={() => auth.signout()}
-            >Sign out </button> 
-            : 
-            <button 
-            onClick={() => auth.signinWithGoogle('/blocks')}
-        >sign in </button>}
-        
+          home
       </div>
   )
 }
